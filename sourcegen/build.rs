6e45649aeb137e3d88a005ef17af4870214b374b
HAN Jingwei <jingwei97@126.com>
2025-01-10 23:00:15 +0800

    add check-cfg to toml to suppress clippy warnings

--- sourcegen/build.rs
+++ sourcegen/build.rs
@@ -1,7 +1,5 @@
 use std::env;
 
-println!("cargo::rustc-check-cfg=cfg(cross_compile)");
-
 fn main() {
     if env::var("HOST") != env::var("TARGET") {
         println!("cargo:rustc-cfg=cross_compile")
